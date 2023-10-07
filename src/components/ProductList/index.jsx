890310f65a88a1365df0b40ba669239af2bc005a
JokerMSD <igorpn2004@gmail.com>
2023-10-07 18:54:49 -0300

    update src/components/ProductList/index.jsx

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,10 +1,10 @@
 import { ProductCard } from "./ProductCard";
 
-export const ProductList = ({ productList }) => {
+export const ProductList = ({ productList, setCartList, addToCart }) => {
    return (
       <ul>
          {productList.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard key={product.id} product={product} setCartList={setCartList} addToCart={addToCart} />
          ))}
       </ul>
    );
