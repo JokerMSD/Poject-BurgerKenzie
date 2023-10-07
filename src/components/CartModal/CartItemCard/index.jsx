2e749c80a2c55cd377024839dae60b9652b544ad
JokerMSD <igorpn2004@gmail.com>
2023-10-07 18:54:46 -0300

    update src/components/CartModal/CartItemCard/index.jsx

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -1,15 +1,19 @@
 import { MdDelete } from "react-icons/md";
 
-export const CartItemCard = ({ product }) => {
-   return (
-      <li>
-         <div>
-            <img src={product.img} alt={product.name} />
-            <h3>{product.name}</h3>
-         </div>
-         <button aria-label="delete" title="Remover item">
-            <MdDelete size={21} />
-         </button>
-      </li>
-   );
+export const CartItemCard = ({ product, removeFromCart }) => {
+  return (
+    <li>
+      <div>
+        <img src={product.img} alt={product.name} />
+        <h3>{product.name}</h3>
+      </div>
+      <button
+        onClick={() => removeFromCart(product)}
+        aria-label="delete"
+        title="Remover item"
+      >
+        <MdDelete size={21} />
+      </button>
+    </li>
+  );
 };
