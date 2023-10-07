36729e5ef5bd18b39402da7cf9ba13b59b1ef254
JokerMSD <igorpn2004@gmail.com>
2023-10-07 19:59:46 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { HomePage } from "./pages/HomePage"
-import "./styles/index.scss"
+import { ToastContainer } from "react-toastify";
+import { HomePage } from "./pages/HomePage";
+import "./styles/index.scss";
 
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
