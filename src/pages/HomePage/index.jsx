07cfa66302237c4b61cd5e9ae21a22b7f401756c
JokerMSD <igorpn2004@gmail.com>
2023-10-07 14:05:22 -0300

    update src/pages/HomePage/index.jsx

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,12 +3,14 @@ import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
 import { foodApi } from "../../services/api";
+import { useEffect } from "react";
 
 export const HomePage = () => {
    const [productList, setProductList] = useState([]);
    const [cartList, setCartList] = useState([]);
 
-   const getProducts = async () => {
+   useEffect(() => {
+      const getProducts = async () => {
       try{
       const response = await foodApi.get("/products")
       const data = response.data
@@ -17,15 +19,20 @@ export const HomePage = () => {
          console.log(error.message);
       }
    };
-
    getProducts();
+}) 
 
-   // useEffect montagem - carrega os produtos da API e joga em productList
-   // useEffect atualização - salva os produtos no localStorage (carregar no estado)
-   // adição, exclusão, e exclusão geral do carrinho
-   // renderizações condições e o estado para exibir ou não o carrinho
-   // filtro de busca
-   // estilizar tudo com sass de forma responsiva
+   useEffect(() => {
+      localStorage.setItem("products", JSON.stringify(productList))
+   },[productList])
+
+
+   // useEffect montagem - carrega os produtos da API e joga em productList               (X)
+   // useEffect atualização - salva os produtos no localStorage (carregar no estado)      (X)
+   // adição, exclusão, e exclusão geral do carrinho                                      ( )
+   // renderizações condições e o estado para exibir ou não o carrinho                    ( )
+   // filtro de busca                                                                     ( )
+   // estilizar tudo com sass de forma responsiva                                         ( )
 
    return (
       <>
