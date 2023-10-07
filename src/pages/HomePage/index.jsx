c5841cead46b78f61bbdfc20b02bbc635731f1a2
JokerMSD <igorpn2004@gmail.com>
2023-10-07 18:54:56 -0300

    update src/pages/HomePage/index.jsx

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -6,30 +6,53 @@ import { foodApi } from "../../services/api";
 import { useEffect } from "react";
 
 export const HomePage = () => {
-   const [productList, setProductList] = useState([]);
-   const [cartList, setCartList] = useState([]);
+   const [productList, setProductList] = useState(() => {
+      const storedProducts = JSON.parse(localStorage.getItem("products"));
+      return storedProducts || [];
+   });
+   const [cartList, setCartList] = useState(() => {
+      const storedCart = JSON.parse(localStorage.getItem("cart"));
+      return storedCart || [];
+   });
 
+   const addToCart = (productToAdd) => {
+      setCartList(cartList => [...cartList, productToAdd]);
+   };
+   
+   const removeFromCart = (productToRemove) => {
+      setCartList(cartList => cartList.filter(product => product.id !== productToRemove.id));
+   };
+   
+   const removeAllFromCart = () => {
+      setCartList([]);
+   };
+   
+   
    useEffect(() => {
       const getProducts = async () => {
-      try{
-      const response = await foodApi.get("/products")
-      const data = response.data
-      setProductList(data);
-      } catch(error) {
-         console.log(error.message);
-      }
-   };
-   getProducts();
-}) 
+         try{
+            const response = await foodApi.get("/products")
+            const data = response.data
+            setProductList(data);
+         } catch(error) {
+            console.log(error.message);
+         }
+      };
+      getProducts();
+   },[]);
 
    useEffect(() => {
+      localStorage.setItem("cart", JSON.stringify(cartList))
+   }, [cartList]);
+   
+   useEffect(() => {
       localStorage.setItem("products", JSON.stringify(productList))
-   },[productList])
-
+   },[productList]);
+   
 
    // useEffect montagem - carrega os produtos da API e joga em productList               (X)
    // useEffect atualização - salva os produtos no localStorage (carregar no estado)      (X)
-   // adição, exclusão, e exclusão geral do carrinho                                      ( )
+   // adição, exclusão, e exclusão geral do carrinho                                      (X)
    // renderizações condições e o estado para exibir ou não o carrinho                    ( )
    // filtro de busca                                                                     ( )
    // estilizar tudo com sass de forma responsiva                                         ( )
@@ -38,8 +61,8 @@ export const HomePage = () => {
       <>
          <Header />
          <main>
-            <ProductList productList={productList} />
-            <CartModal cartList={cartList} />
+            <ProductList productList={productList} setCartList={setCartList} addToCart={addToCart} />
+            <CartModal cartList={cartList} removeFromCart={removeFromCart} removeAllFromCart={removeAllFromCart} />
          </main>
       </>
    );
