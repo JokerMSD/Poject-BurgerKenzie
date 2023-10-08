79231fc2017b3afac608384daa7f364fe8836bbe
JokerMSD <igorpn2004@gmail.com>
2023-10-08 13:18:55 -0300

    update src/components/Header/index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
-import { MdSearch, MdShoppingCart } from "react-icons/md";
+import React from "react";
+import { MdShoppingCart } from "react-icons/md";
 import Logo from "../../assets/Logo.svg";
 import Style from "./style.module.scss";
 
-export const Header = ({cartList,setVisible}) => {
-   const cartNumber = cartList.reduce((prevValue, product) => {
-      return prevValue +  product.quantity;
-    }, 0);
+export const Header = ({ cartList, setVisible }) => {
+  const cartNumber = cartList.reduce((prevValue, product) => {
+    return prevValue + product.quantity;
+  }, 0);
 
-   return (
-      <header className={Style.header}>
-         <img src={Logo} alt="Logo Kenzie Burguer" />
-         <div>
-            <button
-               className={Style.cartOpenButton}
-               onClick={() => setVisible(true)}
-            >
-                <MdShoppingCart size={21} />
-                <span className={Style.cartNumber}>{cartNumber}</span>
-            </button>
-         </div>
-      </header>
-   );
+  return (
+    <header className={Style.header}>
+      <img className={Style.logo} src={Logo} alt="Logo Kenzie Burguer" />
+      <div>
+        <button
+          className={Style.cartOpenButton}
+          onClick={() => setVisible(true)}
+        >
+          <MdShoppingCart size={21} />
+          <span className={Style.cartNumber}>{cartNumber}</span>
+        </button>
+      </div>
+    </header>
+  );
 };
