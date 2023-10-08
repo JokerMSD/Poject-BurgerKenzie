3449fc3c2849e71d67b18d8d7703c7b051db82a0
JokerMSD <igorpn2004@gmail.com>
2023-10-07 21:50:57 -0300

    update src/components/Header/index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { MdSearch, MdShoppingCart } from "react-icons/md";
 import Logo from "../../assets/Logo.svg";
 import Style from "./style.module.scss";
 
-export const Header = () => {
+export const Header = ({cartList,setVisible}) => {
    const [value, setValue] = useState("");
-
+   const cartNumber = cartList.length
    return (
       <header className={Style.header}>
          <img src={Logo} alt="Logo Kenzie Burguer" />
          <div>
-            <button>
+            <button onClick={() => setVisible(true)}>
                 <MdShoppingCart size={21} />
-                <span>0</span>
+                <span>{cartNumber}</span>
             </button>
             <form>
                <input
