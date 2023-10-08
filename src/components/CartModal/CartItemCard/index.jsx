86b6f1188f026a60b3644b4d7df3e868598b2d2e
JokerMSD <igorpn2004@gmail.com>
2023-10-08 01:00:11 -0300

    update src/components/CartModal/CartItemCard/index.jsx

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -1,19 +1,27 @@
 import { MdDelete } from "react-icons/md";
+import Style from "./style.module.scss";
 
 export const CartItemCard = ({ product, removeFromCart }) => {
   return (
     <li>
-      <div>
-        <img src={product.img} alt={product.name} />
-        <h3>{product.name}</h3>
+      <div className={Style.cardcontainer}>
+        <img className={Style.img} src={product.img} alt={product.name} />
+        <div className={Style.info}>
+        <h3 className={Style.name}>{product.name}</h3>
+        <p className={Style.price}>{product.price.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}</p>
+        </div>
+        <button
+          className={Style.removeButton}
+          onClick={() => removeFromCart(product)}
+          aria-label="delete"
+          title="Remover item"
+        >
+          <MdDelete size={21} />
+        </button>
       </div>
-      <button
-        onClick={() => removeFromCart(product)}
-        aria-label="delete"
-        title="Remover item"
-      >
-        <MdDelete size={21} />
-      </button>
     </li>
   );
 };
