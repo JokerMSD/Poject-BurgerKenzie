c8d903a3972b2025f9f374de08574448f0f64cb5
JokerMSD <igorpn2004@gmail.com>
2023-10-08 13:19:02 -0300

    update src/components/ProductList/ProductCard/index.jsx

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -1,22 +1,24 @@
 import Style from "./style.module.scss";
 
-export const ProductCard = ({ product, setCartList, addToCart }) => {
-
-    return(
-        <li className={Style.container}>
-            <section className={Style.imgcontainer}>
-            <img 
-                className={Style.img} 
-                src={product.img} 
-                alt={product.name} 
-            />
-            </section>
-            <div className={Style.productInfo}>
-                <h3>{product.name}</h3>
-                <span>{product.category}</span>
-                <span>{product.price.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
-                <button onClick={() => addToCart(product)} className="buttonMedium">Adicionar</button>
-            </div>
-        </li>
-    )
-}
+export const ProductCard = ({ product, addToCart }) => {
+  return (
+    <li className={Style.container}>
+      <section className={Style.imgcontainer}>
+        <img className={Style.img} src={product.img} alt={product.name} />
+      </section>
+      <div className={Style.productInfo}>
+        <h3 className={Style.nameProduct}>{product.name}</h3>
+        <span className={Style.category} >{product.category}</span>
+        <span className={Style.price} >
+          {product.price.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </span>
+        <button onClick={() => addToCart(product)} className="buttonMedium">
+          Adicionar
+        </button>
+      </div>
+    </li>
+  );
+};
